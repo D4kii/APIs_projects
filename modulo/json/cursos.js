937ae842580d10475987adc8337f2f8cad7cf9d9
DANIELA LINO DA SILVA OLIVEIRA <22282183@dsn127m1242269.cfp127aluno.local>
2023-06-06 13:20:22 -0300

    editando o json de cursos

--- modulo/json/cursos.js
+++ modulo/json/cursos.js
@@ -1,12 +1,12 @@
 var cursos = [
     {
-        "nome"  :   "001 - Técnico em Desenvolvimento de Sistemas",
+        "nome"  :   "Técnico em Desenvolvimento de Sistemas",
         "sigla" :   "DS",
         "icone" :   "https://cdn2.iconfinder.com/data/icons/font-awesome/1792/code-512.png",
         "carga" :   "1200",
     },
     {
-        "nome"  :   "002 - Técnico em Redes de Computadores",
+        "nome"  :   "Técnico em Redes de Computadores",
         "sigla" :   "RDS",
         "icone" :   "https://cdn-icons-png.flaticon.com/512/37/37414.png",
         "carga" :   "1200"
